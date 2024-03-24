5aaa9d0002b463027396eb748e29f94d0a889c27
Luis65Enri <luisargueta475@gmail.com>
2024-03-24 00:15:26 -0600

    22

--- src/configuraciones/config.js
+++ src/configuraciones/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    DB_HOST: process.env.DB_HOST || 'localhost',
-    DB_USER: process.env.DB_USER || 'root',
+    DB_HOST: process.env.DB_HOST || '192.168.58.112',
+    DB_USER: process.env.DB_USER || 'unicah',
     DB_NAME: process.env.DB_NAME || 'db_tienda',
-    DB_PASSWORD: process.env.DB_PASSWORD || 'Luis12345%',
+    DB_PASSWORD: process.env.DB_PASSWORD || 'Unicah123%',
     DB_PORT: process.env.DB_PORT || 3306
 };
