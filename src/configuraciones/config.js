a5260c3ff063910c1ae5f418b533365f58996f9f
Luis65Enri <luisargueta475@gmail.com>
2024-03-24 18:44:40 -0600

    27

--- src/configuraciones/config.js
+++ src/configuraciones/config.js
@@ -1,9 +1,25 @@
 require('dotenv').config();
 
+const DB_HOST = process.env.DB_HOST || 'localhost'
+console.log(`DB_HOST: ${DB_HOST}`);
+
+const DB_USER = process.env.DB_USER || 'root'
+console.log(`DB_USER: ${DB_USER}`);
+
+const DB_NAME = process.env.DB_NAME || 'db_tienda'
+console.log(`DB_NAME: ${DB_NAME}`);
+
+const DB_PASSWORD = process.env.DB_PASSWORD || 'Luis12345%'
+console.log(`DB_PASSWORD: ${DB_PASSWORD}`);
+
+const DB_PORT = process.env.DB_PORT || 3306
+console.log(`DB_PORT: ${DB_PORT}`);
+
 module.exports = {
-    DB_HOST: process.env.DB_HOST || '192.168.58.112',
-    DB_USER: process.env.DB_USER || 'unicah',
-    DB_NAME: process.env.DB_NAME || 'db_tienda',
-    DB_PASSWORD: process.env.DB_PASSWORD || 'Unicah123%',
-    DB_PORT: process.env.DB_PORT || 3306
+    DB_HOST,
+    DB_USER,
+    DB_NAME,
+    DB_PASSWORD,
+    DB_PORT
 };
+
